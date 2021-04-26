faa635bfb0b00adac805dcd772f6ec09907920dd
CheyenneBD <cheyenne.bendor@gmail.com>
2021-04-26 20:10:46 +0300

    Adding DTO classes

---
+++ dataScraping/DTO/Ingredient.py
@@ -0,0 +1,8 @@
+class Ingredient:
+    def __init__(self):
+        self.name = ''
+        self.quantity = 0
+        self.canBeReplaced = False
+        self.Replacement = ''
+
+        #replacement not implemented yet
