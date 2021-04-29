f34c35938dc507afb385bc7024f54aedba416e41
Shani Yacobovitz <shanyacov@gmail.com>
2021-04-29 12:07:54 +0300

    flake8

--- users/forms.py
+++ users/forms.py
@@ -1,17 +1,15 @@
-  
-from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
     # if you want users to have an email field, remove the comment
-    #email = forms.EmailField() 
+    # email = forms.EmailField()
     class Meta:
         model = User
         fields = [
             'username',
-            #'email',
+            # 'email',
             'password1',
             'password2'
         ]
